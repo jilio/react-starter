9c542b9e6408c579f95d4f96eb85153f94c99074
jilio <jilio.stories@gmail.com>
2022-07-23 19:35:01 +1000

    chore: bump packages; bun

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const App = () => (
   <div>
     <h1 className="p-8 text-3xl font-bold">Jilio React Starter</h1>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), reactRefresh()],
+  plugins: [tsconfigPaths(), react()],
 })
