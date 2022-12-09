249ab28b6e329b5a4434184da5f7af9c3cfc12e5
jilio <jilio.stories@gmail.com>
2022-12-09 20:30:38 +1100

    chore: bump packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-const App = () => (
-  <div>
-    <h1 className="p-8 text-3xl font-bold">Jilio React Starter</h1>
-  </div>
-)
-
-export default App
+export default function App() {
+  return (
+    <div>
+      <h1 className="p-8 text-3xl font-bold">Jilio React Starter</h1>
+    </div>
+  )
+}
